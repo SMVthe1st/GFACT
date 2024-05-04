66a7df21759cbcec45c34ac59ad10fe72f130d96
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2024-05-04 18:57:28 -0400

    Update just-use-nmap-bruv.py

--- Python/Port Scanner/just-use-nmap-bruv.py
+++ Python/Port Scanner/just-use-nmap-bruv.py
@@ -5,14 +5,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 scan = input("Enter an address to scan: ")
 
 for x in range(0, 1025):
-  match sock.connect_ex((scan, x)):
-    case 0:
-      print("Port number %s:  Connection open!" % x)
-    case 111:
-      print("Port number %s:  Connection refused" % x)
-    case 106:
-      print("Port number %s:  Already in use" % x)
-    case _:
-      print("Errno: %s" % str(sock.connect_ex((scan, x))))
+  if sock.connect_ex((scan, x)) == 0:
+    print("Port number %s:  Connection open!" % x)
          
 sock.close()
