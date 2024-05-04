a71871d99c2079e5d04279486800624f7723750d
S. M. V <117322465+SMVthe1st@users.noreply.github.com>
2024-05-04 19:00:27 -0400

    Update just-use-nmap-bruv.py

--- Python/Port Scanner/just-use-nmap-bruv.py
+++ Python/Port Scanner/just-use-nmap-bruv.py
@@ -6,6 +6,7 @@ scan = input("Enter an address to scan: ")
 
 for x in range(0, 1025):
   if sock.connect_ex((scan, x)) == 0:
-    print("Port number %s:  Connection open!" % x)
-         
-sock.close()
+    print("Port number %s:  Connection open!" % x)   
+  sock.close()
+  
+print("No available ports")
